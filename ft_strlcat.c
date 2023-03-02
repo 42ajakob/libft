8aeb23508d6fe319bc478d98fea7ea0bbb2b273f
Andrey Jakob <ajakob@3-F-14.42heilbronn.de>
2023-03-02 16:38:54 +0100

    libft

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajakob <ajakob@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 18:46:36 by ajakob            #+#    #+#             */
+/*   Updated: 2022/11/30 15:06:30 by ajakob           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	strnlen(const char *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && i < n)
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srclen;
+	size_t	dstlen;
+
+	srclen = ft_strlen(src);
+	dstlen = strnlen(dst, dstsize);
+	if (dstlen == dstsize)
+		return (dstlen + srclen);
+	ft_strlcpy(&dst[dstlen], src, dstsize - dstlen);
+	return (dstlen + srclen);
+}
